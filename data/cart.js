be9ab2c5718f7c6a89006ddbb2beb686a1b337b6
tkrizak <tomaskrizak@gmail.com>
2024-02-12 17:04:00 +0100

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,17 @@
-export let cart = [
-  {
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-  },
-  {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [
+    {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+    },
+    {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+    },
+  ];
+}
 
 // function to add product to cart
 
@@ -28,6 +32,8 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+
+  saveToStorage();
 }
 
 // function to remove product from cart
@@ -42,4 +48,12 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
+}
+
+//
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
